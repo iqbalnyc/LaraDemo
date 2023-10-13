e0bf4e892b2f49b0d8b28fdfd4ea1493660e8845
Lara Demo <iqbal.dimc@gmail.com>
2023-10-13 19:34:43 -0400

    fifth

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Contact;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Contact::truncate();
+
+        User::factory()->create([
+            'name' => 'habiba'
+        ]);
+
+        $category = Category::factory()->create([
+            'categoryName' => 'Wiring and Cable'
+        ]);
+
+        Product::factory(8)->create([
+            'productCatId' => $category->id
+        ]);
+        
+        
+        // OrderMaster::create(['odrerDate' => now(), 'orderId' => '101', 'userName' => 'saad']);
+        // OrderMaster::create(['odrerDate' => now(), 'orderId' => '102', 'userName' => 'hassan']);
+
+        // OrderDetail::create(['odrerDate' => now(), 'orderId' => '101', 'userName' => 'saad', 'pId' => '1',
+        // 'pName' => 'product name 1', 'pCatId' => '1', 'pPrice' => '99', 'pQty' => '2']);
+
+        // OrderDetail::create(['odrerDate' => now(), 'orderId' => '101', 'userName' => 'haniya', 'pId' => '2',
+        // 'pName' => 'product name 2', 'pCatId' => '1', 'pPrice' => '99', 'pQty' => '2']);
+    }
+}
